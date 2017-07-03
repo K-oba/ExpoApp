9ed24d8772eba848476cc2e64a52cb4039482dc2
Antoni Ramirez <antoniramirezm@gmail.com>
2017-07-03 00:17:03 -0600

    Sprint 3  view user

--- ExpoCR/app/src/main/java/com/kaoba/expocr/constants/Constants.java
+++ ExpoCR/app/src/main/java/com/kaoba/expocr/constants/Constants.java
@@ -9,8 +9,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -22,14 +25,18 @@ public class Constants {
 
     /***Const variables to validate***/
     private static final String URL = "http://10.0.0.147:8080/api/";
-
+    /**
+     *http://10.0.0.147:8080
+     * http://192.168.40.207:8080
+     **/
 
     /**
      * Method to execute post or put request.
-     * @param obj json to send
+     *
+     * @param obj           json to send
      * @param q
-     * @param requestMethod type of request (post or put)
-     * @param finalPath entity to execute (usuarios, stands, etc);
+     * @param requestMethod type of request (post : 1 or put : 2)
+     * @param finalPath     entity to execute (usuarios, stands, etc);
      * @return
      */
     public void executePostPutRequest(final JSONObject obj, RequestQueue q, int requestMethod, String finalPath) throws Exception {
@@ -46,7 +53,7 @@ public class Constants {
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            Log.d("Response",error.getMessage());
+                            Log.d("Response", error.getMessage());
                         }
                     }
             ) {
@@ -61,8 +68,50 @@ public class Constants {
                 }
             };
             queue.add(postRequest);
-        }catch (Exception e){
-            throw  new Exception(e.getMessage());
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
         }
+    }
+
+    public void executeGetRequest(final VolleyCallBack callBack ,RequestQueue q, String finalPath) {
+        RequestQueue queue = q;
+        // prepare the Request
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, URL.concat(finalPath), null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        callBack.onSuccess(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        callBack.onError(error.getMessage());
+                    }
+                }
+        );
+        // add it to the RequestQueue
+        queue.add(getRequest);
+    }
+
+    public void executeGetListRequest(final VolleyCallBack callBack ,RequestQueue q, String finalPath) {
+        RequestQueue queue = q;
+        // prepare the Request
+        JsonArrayRequest getRequest = new JsonArrayRequest(Request.Method.GET, URL.concat(finalPath), null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        callBack.onSuccessList(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        callBack.onError(error.getMessage());
+                    }
+                }
+        );
+        // add it to the RequestQueue
+        queue.add(getRequest);
     }
 }
