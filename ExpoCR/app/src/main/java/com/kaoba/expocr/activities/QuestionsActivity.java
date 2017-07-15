5198c69661dc9c77c321d511bf39cfb669ab30e1
Valeria Ramírez <valeram@koko.cr>
2017-07-15 14:37:59 -0600

    preguntas

---
+++ ExpoCR/app/src/main/java/com/kaoba/expocr/activities/QuestionsActivity.java
@@ -0,0 +1,15 @@
+package com.kaoba.expocr.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kaoba.expocr.R;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+    }
+}
