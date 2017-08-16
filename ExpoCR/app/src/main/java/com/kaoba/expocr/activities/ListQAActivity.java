e508035981d05a9b4027d81b86793e7c8371af35
Valeria Ramírez <valeram@koko.cr>
2017-08-16 17:43:48 -0600

    front fix

--- ExpoCR/app/src/main/java/com/kaoba/expocr/activities/ListQAActivity.java
+++ ExpoCR/app/src/main/java/com/kaoba/expocr/activities/ListQAActivity.java
@@ -1,8 +1,11 @@
 package com.kaoba.expocr.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -12,8 +15,10 @@ import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import com.kaoba.expocr.R;
+import com.kaoba.expocr.Session;
 import com.kaoba.expocr.constants.Constants;
 import com.kaoba.expocr.constants.VolleyCallBack;
+import com.kaoba.expocr.models.ExpositionPOJO;
 import com.kaoba.expocr.models.QAPOJO;
 
 import org.json.JSONArray;
@@ -25,16 +30,31 @@ import java.util.ArrayList;
 public class ListQAActivity extends AppCompatActivity {
     private static final String CHARLA_PATH = "charlasByExpo/";
     private Constants constants;
-
+    //public static final String EXTRA_MESSAGE = "Expo Id";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_qa);
+        ListView charlasList = (ListView) findViewById(R.id.listViewCharlas);
         try {
             getCharlasList();
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        charlasList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+                QAPOJO charlaPOJO = (QAPOJO) parent.getItemAtPosition(position);
+                Intent intent = new Intent(ListQAActivity.this,QuestionsActivity.class);
+                intent.putExtra("charlaId", charlaPOJO.getId().toString());
+                Session session = new Session(getApplicationContext());
+                session.setCharlaId(charlaPOJO.getId());
+                Log.d("CharlaID",session.getCharlaId().toString());
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void getCharlasList() throws JSONException {
@@ -57,14 +77,15 @@ public class ListQAActivity extends AppCompatActivity {
                         QAPOJO charla = new QAPOJO();
                         object = jsonArrayResponse.getJSONObject(i);
                         charla.setName(object.getString("nombre"));
+                        charla.setId(object.getLong("id"));
                         arrayList.add(charla);
                         titlesList[i]=object.getString("nombre");
                         //Log.d("Hola",object.getJSONObject("beacon").toString());
                     }
-                    Log.d("Hola",titlesList[0]);
+                    //Log.d("Hola",titlesList[0]);
                     ListView listview = (ListView) findViewById(R.id.listViewCharlas);
-                    //ArrayAdapter<QAPOJO> adapter = new ArrayAdapter<>(ListQAActivity.this, android.R.layout.simple_list_item_1, arrayList);
-                    ListAdapter adapter = new CustomAdapter(ListQAActivity.this,titlesList);
+                    ArrayAdapter<QAPOJO> adapter = new ArrayAdapter<>(ListQAActivity.this, android.R.layout.simple_list_item_1, arrayList);
+                    //ListAdapter adapter = new CustomAdapter(ListQAActivity.this,titlesList);
                     listview.setAdapter(adapter);
                 } catch (JSONException e) {
                     e.printStackTrace();
